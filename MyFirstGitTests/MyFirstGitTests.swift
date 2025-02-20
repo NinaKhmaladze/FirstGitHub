154332764e31a553788e974a595950723f36bf03
Nina Khmaladze <nina@Ninas-MacBook-Air.local>
2025-02-20 15:34:26 +0400

    First commit

---
+++ MyFirstGitTests/MyFirstGitTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstGitTests.swift
+//  MyFirstGitTests
+//
+//  Created by Nina Khmaladze on 20.02.25.
+//
+
+import Testing
+@testable import MyFirstGit
+
+struct MyFirstGitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
